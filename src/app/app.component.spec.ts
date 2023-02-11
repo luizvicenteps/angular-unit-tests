01fc0dccd437c267f830f3247e7f99b5287a5187
Luiz Vicente Prestes <lvps@cesar.org.br>
2023-02-11 13:57:49 -0400

    Fix httpmodule service test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-unit-tests app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Angular Unit Tests course');
   });
 });
